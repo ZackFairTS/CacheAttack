ffec6a8c0f29099865a77fc5b671c623a0483812
ZackFairTS <402877015@qq.com>
2016-06-25 20:34:17 +0800

    Update attack.h

--- attack.h
+++ attack.h
@@ -2,18 +2,18 @@
 #define __ATTACK_H
 
 #include <stdbool.h>
-#include  <stdint.h>
-#include   <stdio.h>
-#include  <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include  <string.h>
 #include  <signal.h>
 #include  <unistd.h>
-#include   <fcntl.h>
-#include <gmp.h>
-#include <openssl/des.h>
-#include <time.h>
-#include <sys/time.h>
+#include  <fcntl.h>
+#include  <gmp.h>
+#include  <openssl/des.h>
+#include  <time.h>
+#include  <sys/time.h>
 
 #endif
 
